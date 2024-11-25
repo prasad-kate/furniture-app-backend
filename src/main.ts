e6be6341db70c5c73ac599fddb25168910156392
prasadk <prasadkatepk@gmail.com>
2024-11-25 23:12:11 +0530

    feat: add allowed origins to prevent cors error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = new ConfigService();
+
+  const allowedOrigins =
+    configService.get<string>('ALLOWED_ORIGINS')?.split(',') || [];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, Accept',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
