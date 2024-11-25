2523e29fbdbe5a4c2c26b4f58229347abd4f4736
prasadk <prasadkatepk@gmail.com>
2024-11-25 23:12:11 +0530

    feat: add allowed headers to prevent cors error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://localhost:4173',
+      'http://localhost:8081',
+    ], // Allowed URLs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, Accept',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
