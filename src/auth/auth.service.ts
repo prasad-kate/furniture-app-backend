883c2337671e3f8f57f3b5f499024f3f072b38a5
prasad-kate <prasadkatepk@gmail.com>
2024-12-12 12:43:13 +0530

    feat: add user creation api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { CreateNewUserDto, LoginWithEmailDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
 
+  private prisma = new PrismaClient();
+
   // to create a user
-  createNewUser(createNewUserPayload: CreateNewUserDto) {
-    const { email, name } = createNewUserPayload;
-    return {
-      message: 'User created succesfully',
-      email,
-      name,
-    };
+  async createNewUser(createNewUserPayload: CreateNewUserDto) {
+    try {
+      const { email, name, password } = createNewUserPayload;
+
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (existingUser) {
+        throw new ConflictException('Email is taken');
+      }
+
+      const password_hash = await bcrypt.hash(password, 10);
+
+      await this.prisma.user.create({
+        data: {
+          name,
+          email,
+          password_hash,
+        },
+      });
+
+      return {
+        message: 'User created successfully',
+      };
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+
+      console.error('Unexpected error:', error);
+      throw new InternalServerErrorException('Something went wrong');
+    }
   }
 
   // to login user with email and password
