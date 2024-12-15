888c3d1d56773315e1b9f7ada48960a57e776c34
prasadk <prasadkatepk@gmail.com>
2024-12-15 20:16:30 +0530

    fix: return selective data for products api

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,13 +5,26 @@ import { PrismaClient } from '@prisma/client';
 export class ProductsService {
   private prisma = new PrismaClient();
 
+  private readonly productsObj = {
+    id: true,
+    name: true,
+    description: true,
+    image: true,
+    price: true,
+    stars: true,
+    category_id: true,
+  };
+
   async findAll() {
-    return this.prisma.products.findMany();
+    return this.prisma.products.findMany({
+      select: this.productsObj,
+    });
   }
 
   async findOne(id: number) {
     return this.prisma.products.findUnique({
       where: { id },
+      select: this.productsObj,
     });
   }
 
