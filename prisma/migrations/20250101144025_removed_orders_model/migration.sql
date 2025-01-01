c40e41ace9c5e20acc5a8fd118712c556202dbfc
prasadk <prasadkatepk@gmail.com>
2025-01-01 20:11:20 +0530

    fix: removal of orders model

---
+++ prisma/migrations/20250101144025_removed_orders_model/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_user_id_fkey";
+
+-- DropTable
+DROP TABLE "Order";
+
+-- DropEnum
+DROP TYPE "OrderStatus";
